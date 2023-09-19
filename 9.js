536f08960306462b396c833ed310eb54b6b9132e
Ptaha04 <voronaw@gmail.com>
2023-09-20 00:22:57 +0300

    done

--- 9.js
+++ 9.js
@@ -2,9 +2,16 @@ console.log("Завдання: 9 ==============================");
 
 function task9() {
   // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    return Promise.reject(errorMessage);
+  }
   // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
   // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
   // Викликаємо функцію fetchWithError
+  fetchWithError()
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
 }
